f6f162392e7ed5f797208af11a39349fafce4ba4
Juan Duarte <juanduartemartinez@gmail.com>
2023-06-27 18:11:16 -0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 
 import { useState } from 'react'
 import './App.css'
-import dbSpace from './db/quote.json'
+import dbFortune from './db/quote.json'
 import { getRandom } from './utils/random'
 import QuoteBox from './components/QuoteBox'
 
 const bgImages = ['bg1','bg2','bg3','bg4']
 
 function App() {
-  const [quote, setQuote] = useState(getRandom(dbSpace)) // creamos un estad donde se mostrara la frase
+  const [quote, setQuote] = useState(getRandom(dbFortune)) // creamos un estad donde se mostrara la frase
   // le pasamos la funcion que habiamos creado y el valor inicial sera la base que tenemos almacenada
   //para que de ahi escoga la frase aleatoria
   const [bgImage, setBgImage] = useState(getRandom(bgImages))
 
   const handleChangePhrase = () => {
-    setQuote(getRandom(dbSpace))
+    setQuote(getRandom(dbFortune))
     setBgImage(getRandom(bgImages))
   }
-  console.log(quote);
+  
 
   return (
     <main className={`app ${bgImage}`}>
       <section className='app-container'>
         <div className='container-title'>
-        <h1 className='title'>Space Info</h1>
+        <h1 className='title'>Galletas de la Fortuna</h1>
         </div>
         <QuoteBox handleChangePhrase={handleChangePhrase} phrase={quote.phrase} />
 
-        <footer>Fuente: {quote.author} </footer>
+        <footer>Fuente: {phrases.author} </footer>
       </section>
     </main>
   )
